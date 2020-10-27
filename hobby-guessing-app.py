8a3f803153785e0351034155af38aff459ee6716
LOOIENTING07 <70004140+LOOIENTING07@users.noreply.github.com>
2020-10-27 13:35:31 +0800

    Update hobby-guessing-app.py

--- hobby-guessing-app.py
+++ hobby-guessing-app.py
@@ -24,23 +24,31 @@ singing1 = input("I enjoy singing in the shower.")
 singing2 = input("I feel like singing along whenever I hear songs that I know being played.")
 singing3 = input("Singing makes me feel euphoric.")
 
+reading1 = input("I can get lost in the story when reading")
+reading2 = input("I love spending time reading books")
+reading3 = input("I want to write or publish a book of my own in future")
+
 craft_final = int(craft1) + int(craft2) + int(craft3)
 cooking_final = int(cooking1) + int(cooking2) + int(cooking3)
 music_final = int(music1)+ int(music2) + int(music3)
 dancing_final = int(dancing1) + int(dancing2) + int(dancing3)
 singing_final = int(singing1) + int(singing2) + int(singing3)
+reading_final = int(reading1) + int(reading2) + int(reading3)
 
-if craft_final > cooking_final and craft_final > music_final and craft_final > dancing_final and craft_final > singing_final:
+if craft_final > cooking_final and craft_final > music_final and craft_final > dancing_final and craft_final > singing_final and craft_final > reading_final:
   print("We think that you like craft work!")
   
-if cooking_final > craft_final and cooking_final > music_final and cooking_final > dancing_final and cooking_final > singing_final :
+if cooking_final > craft_final and cooking_final > music_final and cooking_final > dancing_final and cooking_final > singing_final and cooking_final > reading_final:
   print("We think that you enjoy cooking!")
 
-if music_final > cooking_final and music_final > craft_final and music_final > dancing_final and music_final > singing_final:
+if music_final > cooking_final and music_final > craft_final and music_final > dancing_final and music_final > singing_final and music_final > reading_final:
   print("We think that you enjoy playing musical instruments!")
   
-if dancing_final > craft_final and dancing_final > cooking_final and dancing_final > music_final and dancing_final > singing_final:
+if dancing_final > craft_final and dancing_final > cooking_final and dancing_final > music_final and dancing_final > singing_final and dancing_final > reading_final:
   print("We think that your hobby is dancing!")
   
-if singing_final > craft_final and singing_final > cooking_final and singing_final > music_final and singing_final > dancing_final:
+if singing_final > craft_final and singing_final > cooking_final and singing_final > music_final and singing_final > dancing_final and singing_final > reading_final:
   print("We think that your hobby is singing!")
+
+if reading_final > craft_final and reading_final > cooking_final and reading_final > music_final and reading_final > dancing_final and reading_final > singing_final:
+  print("We think that your hobby is reading!")
